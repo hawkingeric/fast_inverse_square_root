97fb528c78345c1c74e514d102acba992b91de44
hawkingeric <hawkingeric@gmail.com>
2020-03-31 16:58:33 +0800

    first commit

---
+++ fast_inverse_square_root.cpp
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <iomanip>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+        float number = 2.0;
+        long i;
+        float x2, y;
+        const float threehalfs = 1.5F;
+
+        x2 = number*0.5F;
+        y = number;
+        i = * ( long * ) &y;
+        i = 0x5f3759df - ( i >> 1);
+        y = * (float *) &i;
+        y = y* ( threehalfs - (x2 * y * y));
+        y = y* ( threehalfs - (x2 * y * y));
+        return y;
+
+
+
+}
+
